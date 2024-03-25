833c671a47ab4a79ab84f3df2b5cdee93f0735ea
Helltractor <1664309610@qq.com>
2024-03-25 22:22:53 +0800

    菜品删除功能
    Signed-off-by: Helltractor <1664309610@qq.com>

--- sky-server/src/main/java/com/sky/mapper/DishMapper.java
+++ sky-server/src/main/java/com/sky/mapper/DishMapper.java
@@ -1,8 +1,12 @@
 package com.sky.mapper;
 
+import com.github.pagehelper.Page;
 import com.sky.annotation.AutoFill;
+import com.sky.dto.DishPageQueryDTO;
 import com.sky.entity.Dish;
 import com.sky.enumeration.OperationType;
+import com.sky.vo.DishVO;
+import org.apache.ibatis.annotations.Delete;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Select;
 
@@ -11,7 +15,6 @@ public interface DishMapper {
 
     /**
      * 根据分类id查询菜品数量
-     *
      * @param categoryId
      * @return
      */
@@ -20,9 +23,33 @@ public interface DishMapper {
 
     /**
      * 在菜品表中插入菜品
-     *
      * @param dish
      */
     @AutoFill(value = OperationType.INSERT)
     void insert(Dish dish);
+
+    /**
+     * 菜品分类查询
+     *
+     * @param dishPageQueryDTO
+     * @return DishVO
+     */
+    Page<DishVO> pageQuery(DishPageQueryDTO dishPageQueryDTO);
+
+    /**
+     * 根据id查询菜品
+     *
+     * @param id
+     * @return
+     */
+    @Select("select * from dish where id = #{id}")
+    Dish getById(Long id);
+
+    /**
+     * 根据主键id删除菜品数据
+     *
+     * @param id
+     */
+    @Delete("delete from dish where id = #{id}")
+    void deleteById(Long id);
 }
