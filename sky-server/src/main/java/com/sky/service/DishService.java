50886f2807eb31683341d396cf910ef6af0df974
helltractor <1664309610@qq.com>
2024-07-07 00:17:10 +0800

    套餐管理功能

--- sky-server/src/main/java/com/sky/service/DishService.java
+++ sky-server/src/main/java/com/sky/service/DishService.java
@@ -57,4 +57,11 @@ public interface DishService {
     List<DishVO> listWithFlavor(Dish dish);
 
     void startOrStop(Integer status, Long id);
+    
+    /**
+     * 根据分类id查询菜品
+     * @param categoryId
+     * @return
+     */
+    List<Dish> list(Long categoryId);
 }
