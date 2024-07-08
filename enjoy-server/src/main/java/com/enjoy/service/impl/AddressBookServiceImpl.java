f4fcc9b3ce80adc32557b64e5c765e07cb3372a3
helltractor <1664309610@qq.com>
2024-07-08 15:17:11 +0800

    修改套餐管理功能

--- enjoy-server/src/main/java/com/enjoy/service/impl/AddressBookServiceImpl.java
+++ enjoy-server/src/main/java/com/enjoy/service/impl/AddressBookServiceImpl.java
@@ -14,6 +14,7 @@ import java.util.List;
 @Service
 @Slf4j
 public class AddressBookServiceImpl implements AddressBookService {
+    
     @Autowired
     private AddressBookMapper addressBookMapper;
     
