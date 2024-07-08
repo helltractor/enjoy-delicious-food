6772e15fde46c5060448f0bd382806384e80f094
helltractor <1664309610@qq.com>
2024-07-08 15:17:11 +0800

    修改套餐管理功能

--- enjoy-server/src/main/java/com/enjoy/mapper/SetmealMapper.java
+++ enjoy-server/src/main/java/com/enjoy/mapper/SetmealMapper.java
@@ -19,35 +19,13 @@ public interface SetmealMapper {
     
     /**
      * 根据分类id查询套餐的数量
-     *
      * @param id
-     * @return
      */
     @Select("select count(id) from setmeal where category_id = #{categoryId}")
     Integer countByCategoryId(Long id);
     
     /**
-     * 动态条件查询套餐
-     *
-     * @param setmeal
-     * @return
-     */
-    List<Setmeal> list(Setmeal setmeal);
-    
-    /**
-     * 根据套餐id查询菜品选项
-     *
-     * @param setmealId
-     * @return
-     */
-    @Select("select sd.name, sd.copies, d.image, d.description " +
-            "from setmeal_dish sd left join dish d on sd.dish_id = d.id " +
-            "where sd.setmeal_id = #{setmealId}")
-    List<DishItemVO> getDishItemBySetmealId(Long setmealId);
-    
-    /**
-     * 新增套餐
-     *
+     * 向套餐表插入数据
      * @param setmeal
      */
     @AutoFill(OperationType.INSERT)
@@ -55,36 +33,46 @@ public interface SetmealMapper {
     
     /**
      * 分页查询
-     *
      * @param setmealPageQueryDTO
-     * @return
      */
     Page<SetmealVO> pageQuery(SetmealPageQueryDTO setmealPageQueryDTO);
     
     /**
-     * 根据id查询套餐
-     *
+     * 根据id查询数据
      * @param id
-     * @return
      */
     @Select("select * from setmeal where id = #{id}")
     Setmeal getById(Long id);
     
     /**
-     * 根据id删除套餐
-     *
+     * 根据id删除数据
      * @param setmealId
      */
     @Delete("delete from setmeal where id = #{id}")
     void deleteById(Long setmealId);
     
+    /**
+     * 修改套餐表
+     * @param setmeal
+     */
+    @AutoFill(OperationType.UPDATE)
     void update(Setmeal setmeal);
     
     /**
+     * 条件查询
+     * @param setmeal
+     */
+    List<Setmeal> list(Setmeal setmeal);
+    
+    /**
+     * 根据id查询菜品选项
+     * @param id
+     */
+    List<DishItemVO> getDishItemBySetmealId(Long id);
+    
+    /**
      * 根据条件统计套餐数量
-     *
      * @param map
-     * @return
      */
     Integer countByMap(Map map);
 }
