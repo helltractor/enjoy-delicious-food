4afec65626047817b7711f445d18aac782e68e1b
helltractor <1664309610@qq.com>
2024-07-22 23:49:52 +0800

    配置properties文件，新增demo文件

---
+++ enjoy-common/src/main/java/com/enjoy/properties/AliOssDemo.java
@@ -0,0 +1,21 @@
+package com.enjoy.properties;
+
+import lombok.Data;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+/**
+ * 阿里云OSS配置
+ */
+//@Component
+//@ConfigurationProperties(prefix = "enjoy.alioss")
+//@Data
+//public class AliOssDemo {
+//    private String endpoint;
+//
+//    private String accessKeyId;
+//
+//    private String accessKeySecret;
+//
+//    private String bucketName;
+//}
